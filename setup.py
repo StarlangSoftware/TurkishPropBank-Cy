235e81e185593373d94f52882299fad1e39fb11f
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-02-10 17:18:33 +0300

    Removed case sensitivity bug in ArgumentType

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     ext_modules=cythonize(["PropBank/*.pyx"],
                           compiler_directives={'language_level': "3"}),
     name='NlpToolkit-PropBank-Cy',
-    version='1.0.6',
+    version='1.0.7',
     packages=['PropBank'],
     package_data={'PropBank': ['*.pxd', '*.pyx', '*.c', '*.py']},
     url='https://github.com/StarlangSoftware/TurkishPropbank-Cy',
