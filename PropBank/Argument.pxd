ac145a2da6b7d286c4192286184ae3cc71ca8a23
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2025-02-05 13:33:19 +0300

    Added ArgumentList to suppoer multiple arguments for a word.

--- PropBank/Argument.pxd
+++ PropBank/Argument.pxd
@@ -3,6 +3,8 @@ cdef class Argument(object):
     cdef str __argument_type
     cdef str __id
 
-    cpdef initWithId(self, str argumentType, str _id)
+    cpdef constructor1(self, str argument)
+    cpdef constructor2(self, str argumentType, str _id)
     cpdef str getArgumentType(self)
     cpdef str getId(self)
+    cpdef setId(self, str _id)
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     ext_modules=cythonize(["PropBank/*.pyx"],
                           compiler_directives={'language_level': "3"}),
     name='NlpToolkit-PropBank-Cy',
-    version='1.0.14',
+    version='1.0.15',
     packages=['PropBank', 'PropBank.data'],
     package_data={'PropBank': ['*.pxd', '*.pyx', '*.c', '*.py'],
                   'PropBank.data': ['*.xml']},
